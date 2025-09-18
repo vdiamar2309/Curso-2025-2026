a054274a66f2cb9b5ae6efb7c5575bac28e2a97a
alumno <vdiamar2309@gmail.com>
2025-09-18 12:58:27 +0200

    k

---
+++ src/main/java/sdsdfsdfsdajhXCJKSD.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class sdsdfsdfsdajhXCJKSD {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        Scanner mierda = new Scanner(System.in);
+
+    }
+}
