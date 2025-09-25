aeef2642258499b4fb821feba277b59af591e692
VICTOR <vdiamar2309@gmail.com>
2025-09-25 12:36:57 +0200

    ty
    Signed-off-by: VICTOR <vdiamar2309@gmail.com>

---
+++ src/main/java/TablasMultiplicar1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TablasMultiplicar1 {
+    public static void main(String[] args) {
+        System.out.println("Escribe un número");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int tabla;
+        if (num > 0 && num <= 10) {
+            for (tabla = 0; tabla <= 10; tabla++) {
+                int resultado = tabla * num;
+                System.out.println(num + "x" + tabla + "=" + resultado);
+            }
+        }
+        else{
+            System.out.println("Operación no válida");
+        }
+    }
+}
