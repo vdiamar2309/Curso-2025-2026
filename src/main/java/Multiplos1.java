a98f400fd2e2039b614d0b3fcbf12d3f68255e8d
VICTOR <vdiamar2309@gmail.com>
2025-09-25 13:28:14 +0200

    ty
    Signed-off-by: VICTOR <vdiamar2309@gmail.com>

---
+++ src/main/java/Multiplos1.java
@@ -0,0 +1,15 @@
+import java.io.StreamCorruptedException;
+
+public class Multiplos1 {
+    public static void main(String[] args) {
+        int nums;
+        for (nums = 0; nums <= 100; ) {
+            if (13 % nums == 0) {
+                System.out.println(nums + " es primo");
+
+
+            }
+        }
+    }
+}
+
