7241c15c89bea7b5c123dbf9581ee51f09804570
VICTOR <vdiamar2309@gmail.com>
2025-09-26 08:35:19 +0200

    ty
    Signed-off-by: VICTOR <vdiamar2309@gmail.com>

--- src/main/java/PrimosTrece1.java
+++ src/main/java/PrimosTrece1.java
@@ -2,10 +2,10 @@ public class PrimosTrece1 {
     public static void main(String[] args) {
         int i;
         for (i = 0; i < 100; i++) {
-            if (i % 7 == 1) {
+            if (i % 7 == 0) {
                 System.out.println(i + " Es multiplo de 7");
             }
-            if (i % 13 == 1) {
+            if (i % 13 == 0) {
                 System.out.println(i + " Es multiplo de 13");
             }
         }
