59c35970f1a8225a4cd576911f28425f2af675b2
victo <vdiamar2309@gmail.com>
2025-09-28 23:40:12 +0200

    DIFICILLLLL

---
+++ src/main/java/SumaNPrimeros_4.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class SumaNPrimeros_4 {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        int positivo = 0;
+        do {
+            System.out.println("Escribe un numero positivo");
+            positivo = sc.nextByte();
+            if (positivo < 0) {
+                System.out.println("Numero inválido");
+            }
+        } while (positivo < 0);
+        int suma = 0;
+        for(int bucle = 0;bucle<=positivo;bucle++){
+            suma= suma+bucle;
+            if (bucle==positivo){
+                System.out.println("La suma de todos los numeros es: "+suma);
+            }
+        }
+    }
+}
+
+
