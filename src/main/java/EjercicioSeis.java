69a0909bed24fe7340de41edec9e7d07374cab21
VICTOR <vdiamar2309@gmail.com>
2025-09-30 13:38:09 +0200

    Hola mundo

--- src/main/java/EjercicioSeis.java
+++ src/main/java/EjercicioSeis.java
@@ -1,3 +1,17 @@
-public class EjercicioSeis {
+import java.util.Scanner;
 
+public class EjercicioSeis {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe un número");
+        int a = sc.nextInt();
+        System.out.println("Escribe otro numero");
+        int b = sc.nextInt();
+        int resultado =0;
+        for (int bucle = 0;bucle<Math.abs(b);) {
+        c+a;
+        bucle++;
+        }
+        System.out.println(a+"x"+b+"="+resultado);
+    }
 }
